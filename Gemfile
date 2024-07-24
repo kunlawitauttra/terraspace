66cf61223c993ec6b0b1fb9446e02526524f0810
kunlawitauttra <kunlawit@hotmail.com>
2024-07-25 03:07:31 +0700

    add ci

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ source "https://rubygems.org"
 gem "terraspace", "~> 2.2.0"
 gem "terraspace_plugin_azurerm"
 gem "rspec-terraspace"
+gem "terraspace_ci_github"   # Gets VCS info from CI environment
+gem "terraspace_vcs_github"  # Posts PR comment on GitHub
 
 # Uncomment the ci and vcs provider you wish to use. Should use both ci and vcs gem
 # Docs: https://terraspace.cloud/docs/ci/
--- config/app.rb
+++ config/app.rb
@@ -1,6 +1,8 @@
 # Docs: https://terraspace.cloud/docs/config/reference/
 Terraspace.configure do |config|
-  # config.logger.level = :info
+  config.cloud.org = "my-org"  # replace with your Terraspace Cloud org name
+  config.cloud.project = "infra" # replace with your project name
+  config.cloud.token = ENV['TS_TOKEN']
 
   # copy_modules setting introduced 2.2.5 to speed up terraspace build
   # See: https://terraspace.cloud/docs/config/reference
